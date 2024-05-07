01f286a54f16282239b3cb4cd9b8305e78a97068
ogusam <ogusam32@gmail.com>
2024-05-07 02:57:33 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const nav =document.querySelector('.nav_links');
+const openNavBtn = document.querySelector('#nav_toggle-open');
+const closeNavBtn = document.querySelector('#nav_toggle-close');
+
+const openNav =()=>{
+    nav.style.display ='flex';
+    openNavBtn.style.display ='none';
+    closeNavBtn.style.display ='inline-block';
+}
+
+openNavBtn.addEventListener('click', openNav)
+
+const closeNav =()=>{
+    nav.style.display ='none';
+    openNavBtn.style.display ='inline-block';
+    closeNavBtn.style.display ='none';
+}
+
+closeNavBtn.addEventListener('click', closeNav)
